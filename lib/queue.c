67c2c3004857eb6aaa22940e4efc144e5a7dcb14
Simon Maurer <mail@maurer.systems>
2025-09-17 19:51:41 +0200

    add queue

---
+++ lib/queue.c
@@ -0,0 +1,39 @@
+#include "queue.h"
+
+#include "log.h"
+#include "mem_utils.h"
+
+
+
+void ri_queue_init(ri_queue_t *queue, const ri_channel_param_t *param, uintptr_t start)
+{
+  ri_atomic_index_t *indices = (ri_atomic_index_t *) start;
+
+  *queue = (ri_queue_t) {
+      .n_msgs = ri_calc_queue_len(param),
+      .msg_size = cacheline_aligned(param->msg_size),
+      .tail = &indices[0],
+      .head = &indices[1],
+      .chain = &indices[2],
+      .msgs_start_addr = start + ri_calc_queue_size(param),
+  };
+}
+
+void ri_queue_shm_init(ri_queue_t *queue)
+{
+  atomic_store(queue->tail, RI_INDEX_INVALID);
+  atomic_store(queue->head, RI_INDEX_INVALID);
+}
+
+void ri_queue_dump(ri_queue_t *queue)
+{
+  LOG_INF("\t\tqueue n_msgs=%u, msg_size=%zu", queue->n_msgs, queue->msg_size);
+  LOG_INF("\t\t\ttail[0x%p]=0x%x", queue->tail, *queue->tail);
+  LOG_INF("\t\t\thead[0x%p]=0x%x", queue->head, *queue->head);
+
+  for (unsigned i = 0; i < queue->n_msgs; i++) {
+    LOG_INF("\t\t\tqueue[0x%p]=0x%x", &queue->chain[i], queue->chain[i]);
+  }
+
+  LOG_INF("\t\t\tmsgs_start_addr=0x%p", (void*) queue->msgs_start_addr);
+}
---
+++ lib/queue.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "rtipc.h"
+
+#include "param.h"
+
+#include "index.h"
+
+
+typedef struct ri_queue
+{
+  /* number of messages available, can't be lesser than 3 */
+  unsigned n_msgs;
+  size_t msg_size;
+  uintptr_t msgs_start_addr;
+  /* producer and consumer can change the tail
+    *  the MSB shows who has last modified the tail */
+  ri_atomic_index_t *tail;
+  /* head is only written by producer and only used
+    * in consumer_get_head */
+  ri_atomic_index_t *head;
+  /* circular queue for ordering the messages,
+    * initialized simple as queue[i] = (i + 1) % n,
+    * but due to overruns might get scrambled.
+    * only producer can modify the queue */
+  ri_atomic_index_t *chain;
+} ri_queue_t;
+
+
+
+
+
+static inline void* ri_queue_get_msg(const ri_queue_t *queue, ri_index_t idx)
+{
+  if (idx >= queue->n_msgs)
+    return NULL;
+
+  return (void*) (queue->msgs_start_addr + (idx * queue->msg_size));
+}
+
+
+
+void ri_queue_init(ri_queue_t *queue, const ri_channel_param_t *param, uintptr_t start);
+
+void ri_queue_shm_init(ri_queue_t *queue);
+
+void ri_queue_dump(ri_queue_t *queue);
+
+static inline ri_index_t ri_queue_tail_load(const ri_queue_t *queue)
+{
+  return atomic_load(queue->tail);
+}
+
+static inline void ri_queue_tail_store(const ri_queue_t *queue, ri_index_t val)
+{
+  atomic_store(queue->tail, val);
+}
+
+static inline ri_index_t ri_queue_tail_fetch_or(const ri_queue_t *queue, ri_index_t val)
+{
+  return atomic_fetch_or(queue->tail, val);
+}
+
+static inline bool ri_queue_tail_compare_exchange(const ri_queue_t *queue,
+                                                    ri_index_t expected,
+                                                    ri_index_t desired)
+{
+  return atomic_compare_exchange_strong(queue->tail, &expected, desired);
+}
+
+static inline ri_index_t ri_queue_head_load(const ri_queue_t *queue)
+{
+  return atomic_load(queue->head);
+}
+
+static inline void ri_queue_head_store(const ri_queue_t *queue, ri_index_t val)
+{
+  atomic_store(queue->head, val);
+}
+
+static inline ri_index_t ri_queue_chain_load(const ri_queue_t *queue, ri_index_t idx)
+{
+  return atomic_load(&queue->chain[idx]);
+}
+
+static inline void ri_queue_chain_store(const ri_queue_t *queue,
+                                          ri_index_t idx,
+                                          ri_index_t val)
+{
+  atomic_store(&queue->chain[idx], val);
+}
+
+static inline bool ri_queue_index_valid(const ri_queue_t *queue, ri_index_t idx)
+{
+  return idx < queue->n_msgs;
+}
